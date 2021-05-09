04481be3d4dcb482b9c8c6475d8a261bd4180168
s4nt14go <santiagopergamo@gmail.com>
2021-05-09 12:22:49 -0300

    feat: add table and return data from it when GETting to read-account

--- functions/read-account.ts
+++ functions/read-account.ts
@@ -1,8 +1,27 @@
 import {Context, APIGatewayEvent, APIGatewayProxyHandler} from "aws-lambda";
 import { wrapper } from './lib';
+import * as DynamoDB from 'aws-sdk/clients/dynamodb';
+const DocumentClient = new DynamoDB.DocumentClient();
+
+const { TABLE } = process.env;
 
 export const read: APIGatewayProxyHandler = wrapper(async (event: APIGatewayEvent, _context: Context) => {
   console.log('event:', JSON.stringify(event, null, 2));
 
-  return 'success';
+  const { principalId: account } = event.requestContext.authorizer;
+
+  const { Items } = await DocumentClient.query({
+    TableName: TABLE,
+    KeyConditionExpression: "#name1 = :value1",
+    ExpressionAttributeValues: {
+      ":value1": account,
+    },
+    ExpressionAttributeNames: {
+      "#name1": "account",
+    }
+  }).promise();
+
+  console.log('Items', Items);
+
+  return Items[0] || `No data for account ${account}`;
 });
