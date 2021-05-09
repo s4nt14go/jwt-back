3a1ac320c71e1b44a1629c14498fbb640fb58769
s4nt14go <santiagopergamo@gmail.com>
2021-05-09 08:19:05 -0300

    feat: add get-token function

---
+++ functions/lib.ts
@@ -0,0 +1,23 @@
+export function wrapper(lambda: any) {
+  return async function (event: any, context: any) {
+    let body, statusCode;
+
+    try {
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      console.log('error', e);
+      body = { error: e.message || e.toString()};
+      statusCode = 500;
+    }
+
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+    };
+  };
+}
